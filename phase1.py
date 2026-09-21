b65a55cce43bc3a3ac623687c1d4e2cac0fe662d
Wricheek Bhunia <wricheekbhunia599@gmail.com>
2026-09-22 00:52:09 +0530

    added hf auth

--- phase1.py
+++ phase1.py
@@ -2,8 +2,14 @@ import time
 import threading 
 import torch 
 from transformers import AutoTokenizer, AutoModelForCausalLM,TextIteratorStreamer
+from kaggle_secrets import UserSecretsClient
+from huggingface_hub import login
+user_secrets = UserSecretsClient()
+hf_token = user_secrets.get_secret("HF_KEY")
+login(token=hf_token)
 model_id="meta-llama/Llama-3.2-1B-Instruct"
 print("loading model")
 tokenizer=AutoTokenizer.from_pretrained(model_id)
-model=AutoModelForCausalLM.from_pretrained(model_id,device_map="cuda", torch_dtype=torch.float16, trust_remote_code=True)
+model=AutoModelForCausalLM.from_pretrained(model_id,device_map="auto", torch_dtype=torch.float16, trust_remote_code=True)
 print("model loaded")
+
